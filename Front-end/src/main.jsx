ccc98dcfcfc8a439bd1d15f5bdb22cd01636fcf8
FredMunene <munene2050gitonga@gmail.com>
2025-01-30 15:04:39 +0300

    add wrapper to app for  QueryClientProvider

--- Front-end/src/main.jsx
+++ Front-end/src/main.jsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ThirdwebProvider } from "@thirdweb-dev/react"; 
+import App from "./App.jsx";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "react-query";
 
-createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+console.log(queryClient);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+     <QueryClientProvider client={queryClient}>
+      {/* Wrap the app with QueryClientProvider and pass the queryClient */}
+      <ThirdwebProvider clientId={import.meta.env.VITE_CLIENT_ID}>
+        <App />
+      </ThirdwebProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
