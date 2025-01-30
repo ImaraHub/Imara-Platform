0b641e72975dd5e4eb80612cea7076c21bcdd12c
FredMunene <munene2050gitonga@gmail.com>
2025-01-30 15:07:41 +0300

    update npm dependencies & config files

--- Front-end/vite.config.js
+++ Front-end/vite.config.js
@@ -4,11 +4,17 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {},
+  },
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    include: ['buffer'],
+    exclude: ['lucide-react', 'chunk-J7JDWDWR']
   },
   server: {
     host: true, // Enable listening on all network interfaces
     port: process.env.PORT || 3000, // Use environment port or default to 3000
   },
 });
+
+
