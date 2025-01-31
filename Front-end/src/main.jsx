aa1d69ea0057d826861a89159fd17d9a43f9c6f6
FredMunene <munene2050gitonga@gmail.com>
2025-01-31 08:09:43 +0300

    add thirdweb provider wrapper to app

--- Front-end/src/main.jsx
+++ Front-end/src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
+import  ReactDOM  from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { ThirdwebProvider } from '@thirdweb-dev/react';
 
-createRoot(document.getElementById('root')).render(
+const clientId = import.meta.env.VITE_CLIENT_ID;
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <ThirdwebProvider clientId={clientId}>
     <App />
+    </ThirdwebProvider>
   </StrictMode>
 );
