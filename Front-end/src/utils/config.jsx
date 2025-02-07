e5ec9c9a681cea13b108a6ac78cec7bcd1330b90
FredMunene <munene2050gitonga@gmail.com>
2025-02-07 08:38:46 +0300

    implement token creation function - func accepts token name, symbol, initial supply and url to image

---
+++ Front-end/src/utils/config.jsx
@@ -0,0 +1,40 @@
+import {ethers}  from "ethers";
+import TokenFactoryABI from "../../../backend/out/TokenFactory.sol/TokenFactory.json";
+// import { useAddress } from '@thirdweb-dev/react';
+
+
+export const contractAddress = "0x7E81E4697863cAB4FE4C0d820baCbc9e9843e3dD";
+
+async function createToken({name, symbol, initialSupply, logUrl, address}) {
+    // Initialize ethers.js provider (e.g., using MetaMask's provider)
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+
+    console.log("Creating token with the following parameters:", { name, symbol, initialSupply, logUrl });
+    // print signer address
+    console.log("Signer address:", await signer.getAddress());
+
+    // Initialize contract
+    const tokenFactory = new ethers.Contract(contractAddress, TokenFactoryABI.abi, signer);
+
+    try {
+        const tx = await tokenFactory.createToken(name, symbol, initialSupply, logUrl);
+    
+        const receipt = await tx.wait();  // Wait for transaction to be mined
+        // EXtract token address from emitted event
+        const event = receipt.events?.find(e => e.event === "TokenCreated");
+        
+        if (event === undefined) {
+            console.error("TokenCreated event not found in transaction receipt");
+        }
+        const tokenAddress = event.args[1];
+
+        console.log("Token address:", tokenAddress);
+
+        return tokenAddress;
+    } catch (error) {
+        console.error("Error creating token:", error);
+    }
+}
+
+export default createToken;
