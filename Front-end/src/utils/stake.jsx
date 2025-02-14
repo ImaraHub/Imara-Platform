fc225ec8dcc171df236945265de47d76f19bb1a5
FredMunene <munene2050gitonga@gmail.com>
2025-02-14 11:01:34 +0300

    update stake token function to receive no parameters

--- Front-end/src/utils/stake.jsx
+++ Front-end/src/utils/stake.jsx
@@ -1,33 +1,34 @@
 import {ethers}  from "ethers";
+import stakeFactoryABI from "./stakeAbi.json";
 export const stakeContractAddress = "0x65225a4E25977A00E766dF66269774e5f24b2d55"
 
-async function stakeToken({tokenAddress, amount}) { 
+async function stakeToken() { 
     // Initialize ethers.js provider (e.g., using MetaMask's provider)
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
 
-    console.log("Staking token with the following parameters:", { tokenAddress, amount });
+    console.log("Staking token with the following parameters:");
     // print signer address
     console.log("Signer address:", await signer.getAddress());
 
     // Initialize contract
-    const stakeFactory = new ethers.Contract(stakeContractAddress, StakeFactoryABI.abi, signer);
+    const stakeFactory = new ethers.Contract(stakeContractAddress, stakeFactoryABI.abi, signer);
 
     try {
-        const tx = await stakeFactory.stakeToken(tokenAddress, amount);
+        const tx = await stakeFactory.stake();
     
         const receipt = await tx.wait();  // Wait for transaction to be mined
         // EXtract token address from emitted event
-        const event = receipt.events?.find(e => e.event === "TokenStaked" || e.event === null);
+        // const event = receipt.events?.find(e => e.event === "Staked" || e.event === null);
 
-        if (!event) {
-            console.error("TokenStaked event not found in transaction receipt");
-        }
-        const tokenAddress = event.args[1];
+        // if (!event) {
+        //     console.error("TokenStaked event not found in transaction receipt");
+        // }
+        // const tokenAddress = event.args[1];
 
-        console.log("Token address:", tokenAddress);
+        // console.log("Token address:", tokenAddress);
 
-        return tokenAddress;
+        // return tokenAddress;
     } catch (error) {
         console.error("Error staking token:", error);
     }
