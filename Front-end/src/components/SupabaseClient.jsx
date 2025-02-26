96a2e8298f02f8c0b53256cee7608214336c599e
FredMunene <munene2050gitonga@gmail.com>
2025-02-26 23:14:48 +0300

    feat: add Supabase client for database interactions - implement image upload to Supabase storage - implement image URL retrieval  from Supabase storage

--- Front-end/src/components/SupabaseClient.jsx
+++ Front-end/src/components/SupabaseClient.jsx
@@ -1,16 +1,70 @@
 import { createClient } from '@supabase/supabase-js';
 import React, { useEffect, useState } from 'react';
-import { supabase } from './supabaseClient';
-
+// import { useAuth } from "../AuthContext";
 
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+export const supabase = createClient(supabaseUrl, supabaseKey);
 
-const supabase = createClient(supabaseUrl, supabaseKey);
 
-const DisplayIdeas = (data) => {
+export const addIdea = async (formData, user) => {
+
+    try {
+    
+        // Insert idea along with the authenticated user's UID
+        const { title, details, link, license, image } = formData;
+
+        // Insert Data into Database
+        const { data, error } = await supabase.
+        from('ideas')
+        .insert([{title, details, link, license, image, uid: user.id}]);
+    
+        if (error) {
+          console.error("Insert error:", error);
+          return null;
+        }
+    
+        return data;
+      } catch (err) {
+        console.error("Unexpected error:", err);
+        return null;
+      }
+
+}
+
+export const uploadImageToSupabase = async (image, user) => {
+    // const user = supabase.auth.getUser(); // Check if user is authenticated
+    // console.log("Uploading Image:", image);
+    console.log("Current user in uploadImage:", user);
+
+    // upload image to storage
+
+    const fileExt = image.name.split('.').pop();
+    const fileName = `${Date.now()}.${fileExt}`;
+    const filePath = `${fileName}`;
+
+    // Fetch public URL correctly
+    const { data: publicUrlData, error: urlError } = supabase
+        .storage
+        .from('ideaImages')
+        .getPublicUrl(filePath);
+
+    if (urlError) {
+        console.error("Error retrieving image URL:", urlError);
+        return null;
+    }
+
+    console.log("Public URL Response:", publicUrlData);
+   
+
+    return publicUrlData.publicUrl;
+
+}
+
+export const displayIdeas = () => {
 
     const[data, setData] = useState([]);
+    
 
     useEffect(() => {
         const fetchData = async () => {
@@ -24,18 +78,4 @@ const DisplayIdeas = (data) => {
 
 }
 
-const AddItem = async (title, details, link, license, image) => {
-
-    const { data, error } = await supabase.from('ideas').insert([title, details, link, license, image]);
-
-    if (error) {
-        console.log(error);
-    } else {
-        console.log(data);
-        console.log('Data inserted successfully');
-    }
-
-}
-
-export default SupabaseClient;
 
