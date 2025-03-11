13e1d9920ffed11bd159cfcbad34785d580a2a09
fgitonga <mfredgitonga@gmail.com>
2025-03-11 19:29:26 +0300

    refact app.jsx to handle routing

---
+++ front-end/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Auth from './components/Auth';
+import BuilderProfile from './components/BuilderProfile';
+import CreateIdea from './components/CreateIdea';
+import Index from './components/Index';
+import ProfileSetting from './components/ProfileSettings';
+import ProjectManager from './components/ProjectManager';
+import Stake from './components/stake';
+import TokenizationPage from './components/token';
+import ViewIdea from './components/ViewIdea';
+
+function App() {
+  // const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  return (
+    <Router>
+      <Routes>
+        {/* Redirect unauthenticated users to Auth */}
+
+        <Route path="/" element={<Index />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/builder-profile" element={<BuilderProfile />} />
+        <Route path="/create-idea" element={<CreateIdea />} />
+        <Route path="/ideas" element={<Index />} />
+        <Route path="/profile-settings" element={<ProfileSetting />} />
+        <Route path="/project-manager" element={<ProjectManager />} />
+        <Route path="/stake" element={<Stake />} />
+        <Route path="/token" element={<TokenizationPage />} />
+        <Route path="/view-idea" element={<ViewIdea />} />
+
+      </Routes>
+    </Router>
+
+  )
+}
+
+export default App;
