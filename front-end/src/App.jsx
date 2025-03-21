f94f809a6ac492fd4b6b4b3972de86417d752ffd
fredmunene <munene2050gitonga@gmail.com>
2025-03-21 06:20:57 +0300

    `feat: updated Auth component to include username field, refactored ProfileSettings component to Profile component, and updated SupabaseClient to include new functions for adding and retrieving project contributors`

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -5,7 +5,7 @@ import Auth from './components/Auth';
 import BuilderProfile from './components/BuilderProfile';
 import CreateIdea from './components/CreateIdea';
 import Index from './components/Index';
-import ProfileSetting from './components/ProfileSettings';
+import Profile from './components/Profile';
 import ProjectManager from './components/ProjectManager';
 import Stake from './components/stake';
 import TokenizationPage from './components/token';
@@ -25,7 +25,7 @@ function App() {
         <Route path="/builder-profile" element={<BuilderProfile />} />
         <Route path="/create-idea" element={<CreateIdea />} />
         <Route path="/ideas" element={<Index />} />
-        <Route path="/profile-settings" element={<ProfileSetting />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/project-manager" element={<ProjectManager />} />
         <Route path="/stake" element={<Stake />} />
         <Route path="/token" element={<TokenizationPage />} />
