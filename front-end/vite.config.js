fcaac8d5544d0230275804e46dffe06fb8fb2908
FredMunene <munene2050gitonga@gmail.com>
2025-05-25 15:09:30 +0300

    update deps

--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -4,18 +4,19 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {},
+    'process.env': process.env,
   },
   optimizeDeps: {
     include: ['buffer'],
     exclude: ['lucide-react', 'chunk-J7JDWDWR'],
-    'process.env': process.env,
+    
   },
   server: {
     host: "0.0.0.0", // Allows external access
     port: Number(process.env.PORT) || 3000, // Use Render's expected port
     strictPort: true, // Prevents fallback to another port
     allowedHosts: ['imara-platform-1.onrender.com'], // Allow Render host
+    sourcemap :false,
   },
   preview: {
     host: "0.0.0.0",
