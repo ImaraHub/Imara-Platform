f2e6ad690b49baa43bb82bdebade7d86004bebe6
ALLAN ROBINSON <162472663+Githaiga22@users.noreply.github.com>
2025-06-11 18:56:44 +0300

    Update main.go

--- backend/chat-server/main.go
+++ backend/chat-server/main.go
@@ -8,7 +8,7 @@ import (
 // CORS middleware to allow requests from http://localhost:3000
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "https://www.imarahub.xyz")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		if r.Method == "OPTIONS" {
