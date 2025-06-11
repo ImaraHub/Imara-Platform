46f030b55749ef257dac3ae0cf4ef40e082cd2e3
ALLAN ROBINSON <162472663+Githaiga22@users.noreply.github.com>
2025-06-11 18:57:50 +0300

    Update handlers copy.go

--- backend/chat-server/handlers copy.go
+++ backend/chat-server/handlers copy.go
@@ -15,7 +15,7 @@ var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
 		// Allow only the frontend origin in development. Change for production!
 		origin := r.Header.Get("Origin")
-		return origin == "https://www.imarahub.xyz"
+		return origin == "*"
 	},
 }
 
